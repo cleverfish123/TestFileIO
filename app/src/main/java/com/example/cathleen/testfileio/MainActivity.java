2ed2b317435095970b0c003aa562522b4307d50b
BuildTools <unconfigured@null.spigotmc.org>
2016-09-28 18:09:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cathleen/testfileio/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.cathleen.testfileio;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.*;
+import android.widget.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+    Button write,read;
+    String MyFileName = "myFile.txt";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        write = (Button)findViewById(R.id.write);
+        read = (Button)findViewById(R.id.read);
+
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OutputStream out = null;
+                try {
+                    FileOutputStream fileOutputStream = openFileOutput(MyFileName,MODE_PRIVATE);
+                    out = new BufferedOutputStream(fileOutputStream);
+                    String content="Cathleen"+" "+ "2014011332";
+                    try {
+                    out.write(content.getBytes(StandardCharsets.UTF_8));
+                    }
+                    finally {
+                        if(out!=null)
+                            out.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try {
+                    FileInputStream fileInputStream = openFileInput(MyFileName);
+                    in=new BufferedInputStream(fileInputStream);
+                    int c;
+                    StringBuilder stringBuilder=new StringBuilder("");
+                    try{
+                        while ((c=in.read())!=-1) {
+                            stringBuilder.append((char)c);
+                        }
+                        Toast.makeText(MainActivity.this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+                    }
+                    finally {
+                        if(in!=null)
+                            in.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
